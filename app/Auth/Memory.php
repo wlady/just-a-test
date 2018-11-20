e4e81083cf8303b82cbbbbc0675f8cf4b7cfefb1
Vladimir Zabara <wlady2001@gmail.com>
2018-11-20 22:33:22 +0200

    comments added

--- app/Auth/Memory.php
+++ app/Auth/Memory.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * Very primitive authenticate provider. Do not use it in real projects :)
+ *
  * Created by PhpStorm.
  * User: Kate
  * Date: 20.11.2018
@@ -8,10 +10,15 @@
 
 namespace App\Auth;
 
-
+/**
+ * Class Memory
+ * @package App\Auth
+ */
 class Memory implements IAuth
 {
-
+    /**
+     * Local store
+     */
     private static $store = [
         'admin' => 'admin123',
         'user'  => 'user123',
@@ -22,6 +29,11 @@ class Memory implements IAuth
         session_start();
     }
 
+    /**
+     * @param $name
+     * @param $password
+     * @return bool True on success, False otherwise
+     */
     public function login($name, $password)
     {
         $loggedId = array_key_exists($name, self::$store) && self::$store[$name] == $password;
@@ -32,13 +44,21 @@ class Memory implements IAuth
         return $loggedId;
     }
 
+    /**
+     * We could simply delete this flag
+     */
     public function logout()
     {
         $_SESSION['loggedIn'] = false;
     }
 
+    /**
+     * Check if user is logged in
+     *
+     * @return bool
+     */
     public function check()
     {
-        return $_SESSION['loggedIn'];
+        return $_SESSION['loggedIn'] ?? false;
     }
 }
